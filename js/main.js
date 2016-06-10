e7542383025c3594a4cb8a20e89012cc759183e6
angelo.maramara@gmail.com <Angelo@Angelos-Air.fios-router.home>
2016-06-10 00:59:12 -0400

    Add president objects and array

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
 var iceCream = ['vanilla', 'strawberry', 'pistachio'];
 
 iceCream.push('chocolate');
+
+var president1 = {
+    firstName: 'Barack',
+    lastName: 'Obama',
+    termLength: 2,
+    party: 'Democrat',
+    yearsOfPresidency: 8
+};
+
+var president2 = {
+    firstName: 'George',
+    lastName: 'Bush',
+    termLength: 2,
+    party: 'Republican',
+    yearsOfPresidency: 8
+};
+
+var president3 = {
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    termLength: 2,
+    party: 'Democrat',
+    yearsOfPresidency: 8
+};
+
+var president4 = {
+    firstName: 'George',
+    lastName: 'Bush Sr.',
+    termLength: 1,
+    party: 'Republican',
+    yearsOfPresidency: 4
+};
+
+var president5 = {
+    firstName: 'Ronald',
+    lastName: 'Reagan',
+    termLength: 2,
+    party: 'Republican',
+    yearsOfPresidency: 8
+};
+
+var presidents = [president1, president2, president3, president4, president5];
+
+console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
